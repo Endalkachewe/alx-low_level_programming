829eade79b2cb8843c3b2c9784157fdb13ef3423
Endalkachewe <endalkachew.emare@jku.edu.et>
2023-06-15 19:17:00 -0700

    hello

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - Programming is like building a multilingual puzzle
+* return: always 0 (success)
+*/
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle");
+return (0);
+}
