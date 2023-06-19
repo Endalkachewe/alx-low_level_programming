79ed48882bd09e855616e887c04aa4197414a65d
Endalkachewe <endalkachew.emare@jku.edu.et>
2023-06-19 01:50:54 -0700

    yes

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Prints all possible combinations of two different digits,
  *        in ascending order, separated by a comma followed by a space.
@@ -15,16 +14,12 @@ int main(void)
 		{
 			putchar((a % 10) + '0');
 			putchar((b % 10) + '0');
-
 			if (a == 8 && b == 9)
 				continue;
-			
 			putchar(',');
 			putchar(' ');
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
