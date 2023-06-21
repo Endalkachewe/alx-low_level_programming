c6dcadf4881bc4f0e6ce26b61611bf73037dab2f
Endalkachewe <endalkachew.emare@jku.edu.et>
2023-06-20 18:58:22 -0700

    yes

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet - prints alphabets in lower case
+ * 
+ * Return: Always 0 (successful)
+ */
+void print_alphabet(void)
+{
+	int i;
+	for (i =97; i<122; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
