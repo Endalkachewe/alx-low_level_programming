1c25ea69b1c17d03c7e4b1e4af5950d900a47c9c
Endalkachewe <endalkachew.emare@jku.edu.et>
2023-07-04 18:52:17 -0700

    All questions

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_chessboard - Entry point
+ * @a: array
+ * Return: Always 0 (Success)
+ */
+void print_chessboard(char (*a)[8])
+{
+	int k;
+	int l;
+
+	for (l = 0; l < 8; l++)
+	{
+		for (k = 0; k < 8; k++)
+			_putchar(a[l][k]);
+		_putchar('\n');
+	}
+}
